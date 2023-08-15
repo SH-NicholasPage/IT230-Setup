46a0da84b2aad5b568bed95a207a8db90acf9c1b
Nicholas Page <npage@southhills.edu>
2023-08-15 17:50:21 -0400

    Initial commit

---
+++ CppTest/main.cpp
@@ -0,0 +1,18 @@
+/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std; // Bring the names from the std namespace into scope
+
+//Replace the string's value with your name
+const string YOUR_NAME = "[YOUR NAME HERE]";
+
+int main()
+{
+    cout << "Hello, " << YOUR_NAME << "!" << endl;
+    return 0;
+}
---
+++ CsTest/CsTest/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Name: [YOUR NAME HERE]
+ * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+ */
+
+using System;
+
+namespace CsTest
+{
+    public class Program
+    {
+        //Replace the string's value with your name
+        private const String NAME = "[YOUR NAME HERE]";
+        
+        public static void Main()
+        {
+            Console.WriteLine($"Hello, {NAME}!");
+        }
+    }
+}
